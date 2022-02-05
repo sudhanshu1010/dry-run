029a685cb73d525104de5daf5f8fcd0ccd39cc68
Sudhanshu Kumar <sudhanshukumar1010@gmail.com>
2022-02-05 12:58:48 +0530

    update

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,33 +1,84 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './Login.module.css'
-import {MdAlternateEmail} from 'react-icons/md'
-import {FiKey} from 'react-icons/fi'
+import { MdAlternateEmail } from 'react-icons/md'
+import { FiKey } from 'react-icons/fi'
+import { AiOutlineClear, AiOutlineEye, AiOutlineEyeInvisible } from 'react-icons/ai'
 
 const Login = () => {
+    const [showPassword, setShowPassword] = useState(false)
+    const [emailError, setEmailError] = useState(false)
+    const [passwordError, setPasswordError] = useState(false)
+
+    const [email, setEmail] = useState('')
+    const [password, setpassword] = useState('')
+
+
+    const validateEmail = (email) => {
+        var isValid = String(email)
+          .toLowerCase()
+          .match(
+            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+          );
+
+          return isValid
+      };
+
+    function handleEmail(emailValue) {
+        var isValid = validateEmail(emailValue)
+        setEmail(emailValue)
+
+        if(!isValid){
+            setEmailError(true)
+        } else setEmailError(false)
+    }
+
     return (
         <div className={styles.login_main_container}>
             <div className={styles.login_form_container}>
                 <div className={styles.login_header}>Login</div>
-                <div className={styles.email_container}>
-                    <MdAlternateEmail/>
-                    <input type="email" placeholder="Email" className={styles.email}/>
+                <div className={styles.input_container}>
+                    <div className={styles.input_logo_container}>
+                        <MdAlternateEmail title='Enter your email address' />
+                    </div>
+                    <input
+                        type="email"
+                        placeholder="Email"
+                        className={styles.email}
+                        onChange={e => handleEmail(e.target.value)}
+                        value={email}
+                    />
+                    <div className={styles.options_container}>
+                        <AiOutlineClear title="Clear email" onClick={() => setEmail('')} />
+                    </div>
                 </div>
+                {
+                    emailError ? <small>Enter a valid email address!</small> : ''
+                }
 
-                <div className={styles.password_container}>
-                    <FiKey/>
-                    <input type="password" placeholder="Password" className={styles.password}/>
+                <div className={styles.input_container}>
+                    <div className={styles.input_logo_container}>
+                        <FiKey title='Enter your password' />
+                    </div>
+
+                    <input type={showPassword ? 'text' : 'password'} placeholder="Password" className={styles.password} />
+                    <div className={styles.options_container} onClick={() => setShowPassword(!showPassword)}>
+                        {!showPassword ? <AiOutlineEyeInvisible /> : <AiOutlineEye />}
+                    </div>
                 </div>
+                {
+                    passwordError ? <small>Password is not correct!</small> : ''
+                }
 
                 <div className={styles.remember_container}>
                     <input type="checkbox" placeholder="Remember"/>
-                    Remember me
+                    <small>Remember me</small>
                 </div>
 
                 <button>Login</button>
 
-                <hr/>
+                <hr />
 
-                
+
             </div>
         </div>
     )
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -7,21 +7,36 @@ import { AiOutlineBell } from 'react-icons/ai'
 import { GoSignOut } from 'react-icons/go'
 
 const Navbar = () => {
-    const [showProfileLinks, setProfileLinks] = useState(false)
+    const [userIcons, userIconsClicked] = useState(false)
     const [isProfileClicked, setProfileClicked] = useState(false)
+    const [isSignoutClicked, setSignoutClicked] = useState(false)
 
     const node = useRef()
 
     useEffect(() => {
-        const handleClick = () => {
-            setProfileLinks(false)
+        const handleClick = (e) => {
+            if(userIcons && node.current && !node.current.contains(e.target)){
+                userIconsClicked(false)
+            }
         }
         document.addEventListener('mousedown', handleClick)
 
         return () => {
             document.removeEventListener('mousedown', handleClick)
         };
-    }, [])
+    }, [userIcons])
+
+    function handleProfile() {
+        setProfileClicked(true)
+        userIconsClicked(false)
+        console.log('profile')
+    }
+
+    function handleSignout() {
+        setSignoutClicked(true)
+        userIconsClicked(false)
+        console.log('Signing out')
+    }
 
     return (
         <div ref={node} className={styles.navbar_main_container}>
@@ -34,16 +49,22 @@ const Navbar = () => {
             </div>
             <div className={styles.navbar_right_container}>
                 <div className={styles.user_icons_container}>
-                    <CgProfile title="User profile" onClick={() => { setProfileLinks(!showProfileLinks) }} />
+                    <CgProfile title="User profile" onClick={() => { userIconsClicked(!userIcons) }} />
                     <AiOutlineBell title="Notifications" />
                 </div>
                 {
-                    showProfileLinks ? (
+                    userIcons && (
                         <div className={styles.profile_links_container} >
-                            <div ><Dropdown icon={<CgProfile />} detail={'Profile'} /></div>
-                            <div><Dropdown icon={<GoSignOut />} detail={'Sign Out'} /></div>
+                            <div className={styles.dropdown_main_container} onClick={() => handleProfile()} >
+                                <div className={styles.dropdown_icon}><CgProfile /></div>
+                                <div className={styles.dropdown_detail}>Profile</div>
+                            </div>
+                            <div className={styles.dropdown_main_container} onClick={() => handleSignout()}>
+                                <div className={styles.dropdown_icon}><GoSignOut /></div>
+                                <div className={styles.dropdown_detail}>Sign Out</div>
+                            </div>
                         </div>
-                    ) : <></>
+                    )
                 }
             </div>
         </div>
